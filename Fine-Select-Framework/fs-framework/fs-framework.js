8376776eec5ec214d88fe0a782555ac0de93e621
mlWebDeveloper <mlwebdeveloper@outlook.com>
2017-02-26 14:22:17 -0500

    Update fs-framework.js

--- Fine-Select-Framework/fs-framework/fs-framework.js
+++ Fine-Select-Framework/fs-framework/fs-framework.js
@@ -1,19 +1,18 @@
 // fs-framework.js
-
 var fs = {
-	get_id : function (el) {
+	getId : function (el) {
 		return document.getElementById(el);
 	},
-	get_tag : function (el) {
+	getTag : function (el) {
 		return document.getElementsByTagName(el)[0];
 	},
-	get_class : function (el) {
+	getClass : function (el) {
 		return document.getElementsByClassName(el)[0];
 	},
-	get_name : function (el) {
+	getName : function (el) {
 		return document.getElementsByName(el)[0];
 	},
-	cr_el : function (el){ 
+	crEl : function (el){ 
 		return document.createElement(el);
 	}
 }
