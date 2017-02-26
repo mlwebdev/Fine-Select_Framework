3106d875f0855d2ba5bc9395b2823ca92c774bf5
mlWebDeveloper <mlwebdeveloper@outlook.com>
2017-02-26 16:55:58 -0500

    Update fs-framework.js

--- Fine-Select-Framework/fs-framework/fs-framework.js
+++ Fine-Select-Framework/fs-framework/fs-framework.js
@@ -1,21 +1,28 @@
 // fs-framework.js
-var fs = {
-	getId : function (el) {
-		return document.getElementById(el);
-	},
-	getTag : function (el) {
-		return document.getElementsByTagName(el)[0];
-	},
-	getClass : function (el) {
-		return document.getElementsByClassName(el)[0];
-	},
-	getName : function (el) {
-		return document.getElementsByName(el)[0];
-	},
-	crEl : function (el){ 
-		return document.createElement(el);
-	}
-}
+window.addEventListener("DOMContentLoaded", function doItAll() {
+	const fs={getId:function(a){return document.getElementById(a)},getTag:function(a){return document.getElementsByTagName(a)[0]},
+	getClass:function(a, i){return document.getElementsByClassName(a)[i]},getName:function(a){return document.getElementsByName(a)[0]},crEl:function(a){return document.createElement(a)}};
+
+/*	
+	var _body = fs.getTag("body");
+	_body.innerHTML = "<h2>Yes !! You Good</h2>";
+*/
+
+	var _yClass = fs.getClass("yClass", 0);
+	_yClass.innerHTML = "<h3>Oh Yes !! You Good</h3>" +
+		"<p>This is the first element with the yClass</p>";
+
+	var _yClass2 = fs.getClass("yClass", 1);
+	_yClass2.innerHTML = "<h4>Oh, Oh Yes !! You Real Good</h4>" +
+		"<p>This is the second element with the yClass</p>";
+
+	var _zClass = fs.getClass("zClass", 0);
+	_zClass.innerHTML = "<h4>Man !! You know you're Good</h4>" +
+		"<p>This is the first element with the yClass</p>";
+
+});
+
+
 
 
 
