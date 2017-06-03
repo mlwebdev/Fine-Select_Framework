5dbf691654209e2043d492dd7e6f4b08c3897b16
mlWebDeveloper <mlwebdeveloper@outlook.com>
2017-06-02 23:15:30 -0400

    Update fs-framework.js

--- Fine-Select-Framework/fs-framework/fs-framework.js
+++ Fine-Select-Framework/fs-framework/fs-framework.js
@@ -1,5 +1,5 @@
 // fs-framework.js
-(function(getId,getTag,getClass,getName,crEl){this.getId=function(a){return document.getElementById(a)},this.getTag=function(a){return document.getElementsByTagName(a)[0]},this.getClass=function(a,i){return document.getElementsByClassName(a)[i]},this.getName=function(a){return document.getElementsByName(a)[0]},this.crEl=function(a){return document.createElement(a)}})();
+(function(getId,getTag,getClass,getName,crEl, setItem){this.getId=function(a){return document.getElementById(a)},this.getTag=function(a){return document.getElementsByTagName(a)[0]},this.getClass=function(a,i){return document.getElementsByClassName(a)[i]},this.getName=function(a){return document.getElementsByName(a)[0]},this.crEl=function(a){return document.createElement(a)},this.setItem=function(a, b) {return localStorage.setItem(a,b)}})();
 	
 	var _body = getTag("body");
 	_body.innerHTML = "<h2>Yes !! You Good</h2>";
