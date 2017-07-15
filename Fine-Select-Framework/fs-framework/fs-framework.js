efbd93691aade5e950364f31b5ffe80c610e647a
mlWebDeveloper <mlwebdeveloper@outlook.com>
2017-07-15 12:19:07 -0400

    Update fs-framework.js

--- Fine-Select-Framework/fs-framework/fs-framework.js
+++ Fine-Select-Framework/fs-framework/fs-framework.js
@@ -1,6 +1,16 @@
 // fs-framework.js
-(function(getId,getTag,getClass,getName,crEl, setItem){this.getId=function(a){return document.getElementById(a)},this.getTag=function(a){return document.getElementsByTagName(a)[0]},this.getClass=function(a,i){return document.getElementsByClassName(a)[i]},this.getName=function(a){return document.getElementsByName(a)[0]},this.crEl=function(a){return document.createElement(a)},this.setItem=function(a, b) {return localStorage.setItem(a,b)}})();
-	
-
-});
-
+(function(getId, getTag, getClass, getName, crEl, setItem) {
+  this.getId = function(a) {
+    return document.getElementById(a)
+  }, this.getTag = function(a) {
+    return document.getElementsByTagName(a)[0]
+  }, this.getClass = function(a, i) {
+    return document.getElementsByClassName(a)[i]
+  }, this.getName = function(a) {
+    return document.getElementsByName(a)[0]
+  }, this.crEl = function(a) {
+    return document.createElement(a)
+  }, this.setItem = function(a, b) {
+    return localStorage.setItem(a, b)
+  }
+})();
